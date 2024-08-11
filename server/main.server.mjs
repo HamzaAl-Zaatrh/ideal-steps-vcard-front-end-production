b4733ba342e7c3b70b61b63c08008122a4abf45a
HamzaAl-Zaatreh <hamzazaatreh20@gmail.com>
2024-08-11 14:41:24 +0300

    add all file v1.0.0

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-32IIYAQI.mjs";import"./chunk-5NOFXQGP.mjs";import"./chunk-2ZFPXKYH.mjs";import"./chunk-VMORKJ7Y.mjs";import"./chunk-MCMJUPWD.mjs";import"./chunk-DCLF3XZT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
