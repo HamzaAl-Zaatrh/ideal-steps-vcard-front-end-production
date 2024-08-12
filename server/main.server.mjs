2f6b0680be20065f5c45c84a97657a4e7b4e4091
HamzaAl-Zaatreh <hamzazaatreh20@gmail.com>
2024-08-12 22:43:48 +0300

    add 404 page and add google anayltics

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WTEI23FF.mjs";import"./chunk-RP2PQLNB.mjs";import"./chunk-TCU2IXNV.mjs";import"./chunk-MKI7Z2WJ.mjs";import"./chunk-G7BVTDIC.mjs";import"./chunk-LPUJ23VI.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-63G3JLPS.mjs";import"./chunk-EEDGRRKI.mjs";import"./chunk-FBPREEBK.mjs";import"./chunk-CJN456KB.mjs";import"./chunk-JCINJUNE.mjs";import"./chunk-6J25YNQ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
